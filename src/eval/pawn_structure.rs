de78a66bc2ecabb31c7f37518a51900543be9913
sunspcts <crynglghtng@gmail.com>
2026-08-09 02:22:15 +0100

    Add is_passed to pawn_structure.rs

--- src/eval/pawn_structure.rs
+++ src/eval/pawn_structure.rs
@@ -20,8 +20,60 @@ const ADJACENT_FILE_MASKS: [Bitboard; 8] = [
     Bitboard::new(FILE_G)
 ];
 
+const fn generate_passed_pawn_mask(color: usize, sq: usize) -> u64 {
+    let rank = sq / 8;
+    let file = sq % 8;
+    let mut mask: u64 = 0;
+
+    if color == 0 {
+        let mut r = rank + 1;
+        while r < 8 {
+            mask |= 1u64 << (r * 8 + file);
+            if file > 0 {
+                mask |= 1u64 << (r * 8 + file - 1);
+            }
+            if file < 7 {
+                mask |= 1u64 << (r * 8 + file + 1);
+            }
+            r += 1;
+        }
+    } else {
+        let mut r = 0;
+        while r < rank {
+            mask |= 1u64 << (r * 8 + file);
+            if file > 0 {
+                mask |= 1u64 << (r * 8 + file - 1);
+            }
+            if file < 7 {
+                mask |= 1u64 << (r * 8 + file + 1);
+            }
+            r += 1;
+        }
+    }
+    mask
+}
+
+pub const PASSED_PAWN_MASKS: [[Bitboard; 64]; 2] = {
+    let mut masks = [[Bitboard::zero(); 64]; 2];
+    let mut color = 0;
+    while color < 2 {
+        let mut sq = 0;
+        while sq < 64 {
+            masks[color][sq] = Bitboard::new(generate_passed_pawn_mask(color, sq));
+            sq += 1;
+        }
+        color += 1;
+    }
+    masks
+};
+
 #[inline]
 pub fn is_isolated(sq: u16, friendly_pawns: Bitboard) -> bool {
     let file = (sq & 0b0111) as usize;
     (friendly_pawns & ADJACENT_FILE_MASKS[file]) == Bitboard::zero()
+}
+
+#[inline]
+pub fn is_passed(sq: u16, color: usize, enemy_pawns: Bitboard) -> bool {
+    (PASSED_PAWN_MASKS[color][sq as usize] & enemy_pawns) == Bitboard::zero()
 }

329239ea667fe5d04d87e38df9a89ac283c1fb5b
sunspcts <crynglghtng@gmail.com>
2026-08-12 01:46:27 +0100

    Accidentally implementing MVV-MVA instead of MVV-LVA is rather foolish, huh.

--- src/heuristics.rs
+++ src/heuristics.rs
@@ -1,6 +1,7 @@
 use crate::piece::Piece;
 
-const MVV_LVA_TAB: [[i16; 7]; 7] = init_mvv_lva_table();
+// The table is 7x7 rather than 6x6 because score_qsearch_moves checks it unconditionally.
+const MVV_LVA_TAB: [[i16; 7]; 7] = init_mvv_lva_table(); 
 
 const fn init_mvv_lva_table() -> [[i16; 7]; 7] {
     let mut tab = [[0; 7]; 7];
@@ -8,7 +9,7 @@ const fn init_mvv_lva_table() -> [[i16; 7]; 7] {
     while a < 6 {
         let mut v = 0;
         while v < 6 {
-            tab[v][a] = (((v + 1) * 10) - (6 - a)) as i16;
+            tab[v][a] = (((v + 1) * 10) + (6 - a)) as i16;
             v += 1;
         }
         a += 1;
@@ -16,7 +17,7 @@ const fn init_mvv_lva_table() -> [[i16; 7]; 7] {
     tab
 }
 
-#[inline(always)]
+#[inline]
 pub fn calc_mvv_lva_heuristic(piece: Piece, enemy_piece: Piece) -> i16 {
     MVV_LVA_TAB[enemy_piece as usize][piece as usize]
 }
